f452220579c5a90eb2931684f5c3c10e5ddcbb40
BLEER-w <abutbollyric07@gmail.com>
2026-04-16 15:12:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const API_KEY = '3bd4b23b8db71c70de8380ebc7f4bccb';
 
 const SPOTIFY_CLIENT_ID = "d1d9a544e07a4dd999fe4cd3dbf26ebd";
 const SPOTIFY_CLIENT_SECRET = "0d05c8793cb846e3a8a17519269458dd";
+const YOUTUBE_API_KEY = "AIzaSyAZ2twXaUCGHKvSGVVhEdy57dbUGVIswsY";
 
 let spotifyToken = null;
 let currentAudio = null;
@@ -69,9 +70,17 @@ function getArtistImage(a) {
     // fallback (always works)
     return `https://placehold.co/300x300/1a003d/ffffff?text=${encodeURIComponent(a.name)}`;
 }
-function getYouTubeEmbedUrl(artist, song) {
-    const query = encodeURIComponent(`${artist} ${song}`);
-    return `https://www.youtube.com/embed?autoplay=1&rel=0&modestbranding=1&controls=1&showinfo=0&listType=search&list=${query}`;
+async function getYouTubeVideoId(artist, song) {
+    const query = encodeURIComponent(`${artist} ${song} official music video`);
+
+    const res = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${YOUTUBE_API_KEY}&type=video&maxResults=1`
+    );
+
+    const data = await res.json();
+
+    return data.items?.[0]?.id?.videoId || null;
+}
 }
 
 function openSpotify(name) {
@@ -161,26 +170,32 @@ async function search() {
         </div>
     `;
 
-    if (track?.preview) {
+   if (track?.preview) {
 
     const videoBtn = card.querySelector(".video-btn");
     const container = card.querySelector(".video-container");
 
-    videoBtn.onclick = (e) => {
+    videoBtn.onclick = async (e) => {
         e.stopPropagation();
 
-        // toggle
         if (container.style.display === "block") {
             container.innerHTML = "";
             container.style.display = "none";
             return;
         }
 
-        const url = getYouTubeEmbedUrl(a.name, track.name);
+        container.innerHTML = "Loading video...";
+
+        const videoId = await getYouTubeVideoId(a.name, track.name);
+
+        if (!videoId) {
+            container.innerHTML = "<p>No video found</p>";
+            return;
+        }
 
         container.innerHTML = `
             <iframe width="100%" height="200"
-                src="${url}"
+                src="https://www.youtube.com/embed/${videoId}"
                 frameborder="0"
                 allow="autoplay; encrypted-media"
                 allowfullscreen>
